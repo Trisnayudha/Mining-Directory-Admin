1394088874be7c3cc883a1a42fb56c8899a62d1c
Trisnayudha <trisnayuhabachtiar@gmail.com>
2024-05-08 15:21:58 +0700

    migrate users

---
+++ database/migrations/2024_05_08_061049_create_users.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->boolean('marketing')->nullable()->default(false);
+            $table->boolean('explore')->nullable()->default(false);
+            $table->boolean('is_verified')->nullable()->default(false);
+            $table->string('verification_token')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
